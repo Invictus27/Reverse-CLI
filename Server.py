54bb878c6407186d7e53818501897e8ca07166e7
Morta1DieTw1Ce <134328872+Morta1DieTw1Ce@users.noreply.github.com>
2023-10-01 02:39:38 +0530

    Update Server.py
    Added Authentication system and input sanitization

--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 import socket
+import shlex
 
 def start():
     GREEN = '\033[92m'
@@ -7,12 +8,14 @@ def start():
 
     host = '0.0.0.0'
     port = 8080
+    keys = ['secretkey1', 'secretkey2', 'secretkey3'] 
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.bind((host, port))
         s.listen(5)
         print(f'               {GREEN}      Reverse-CLI   {RESET} \n')
-        print(f'{GREEN}Developer: Morta1DieTw1Ce /n{RESET}')
+        print(f'{GREEN}Developer: Morta1DieTw1Ce \n{RESET}')
         print(f'{GREEN}[*] Listening on {host}:{port}{RESET} ')
     except Exception as e:
         print(f'{RED}Error: Reverse shell not working! {e}{RESET}')
@@ -21,6 +24,12 @@ def start():
     while True:
         conn, addr = s.accept()
         print(f'{GREEN}{conn} has connected\n\n{RESET}')
+        client_key = conn.recv(1024).decode()
+        if client_key not in keys:
+            print(f'{RED}Authentication failed for {conn}{RESET}')
+            conn.close()
+            continue
+
         print(f'{GREEN}     Options    {RESET}\n ')
         print(f'{GREEN}[*] ls\n[*] netstat\n[*] ifconfig\n[*] ps\n[*] df\n[*] whoami\n[*] exit{RESET}')
         while True:
@@ -30,6 +39,7 @@ def start():
                 conn.close()
                 break
             elif cmd in ['ls', 'netstat', 'ifconfig', 'ps', 'df', 'whoami']:
+                cmd = shlex.quote(cmd)
                 conn.sendall(cmd.encode())
                 response = conn.recv(1024).decode()
                 print(response)
